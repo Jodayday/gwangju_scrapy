116be12943152fa33fe9b886d76e6771334774af
joday <iddqfd5@naver.com>
2022-06-27 15:49:52 +0900

    db 연결후 조회

--- newscrawl/newscrawl/spiders/collection.py
+++ newscrawl/newscrawl/spiders/collection.py
@@ -1,7 +1,18 @@
+import os
+import json
 import scrapy
+import MySQLdb
+from MySQLdb.connections import Connection
+from pathlib import Path
 from scrapy.http.response.html import HtmlResponse
-
 from newscrawl.items import NewsItem
+
+
+# db json 불러오기
+KEY_DIR = Path(__file__).resolve().parent.parent.parent
+KEY_PATH = Path.joinpath(KEY_DIR, 'dbkey.json')
+KEY_DICT = json.loads(open(KEY_PATH, 'r').read())
+DB = KEY_DICT['DB']
 
 
 class CollectionSpider(scrapy.Spider):
@@ -14,35 +25,31 @@ class CollectionSpider(scrapy.Spider):
 
     def start_requests(self):
         """db에서 값을 가져오고 크롤링 진행 """
-        names = ['광주시청']
-        start_urls = [
-            'https://www.gwangju.go.kr/boardList.do?boardId=BD_0000000022&pageId=www788']
-        select1s = [
-            'div.board_list_body > div.body_row > div.subject > a::attr("href")']
-        select2s = ['h6::text']
-        select3s = ['div.board_list_body > div.body_row > div.date::text ']
+        conn : Connection = MySQLdb.connect(host=DB['host'],user=DB['user'],password=DB['password'],database=DB['database'],port=DB['port'])
+        sql = "SELECT * FROM CrawlLists "
+        cur = conn.cursor()
+        cur.execute(sql)
+        result = cur.fetchall()
+        conn.close()
 
         # 시작
-        for name, url, s1, s2, s3 in zip(names, start_urls, select1s, select2s, select3s):
+        for _, name, url, s1, s2, s3, _ in result:
             yield scrapy.Request(url=url, callback=self.parse, cb_kwargs=dict(name=name, s1=s1, s2=s2, s3=s3))
 
     def parse(self, response: HtmlResponse, name, s1, s2, s3):
         """공지사항 파싱"""
-        print("값 테스트", name, s1, s2, s3)
-
         links = response.css(s1).getall()
         times = response.css(s3).getall()
-        print("크롤링 된값", links, times)
-    #     for link, time in zip(links, times):
-    #         url = response.urljoin(link)
-    #         yield scrapy.Request(url=url, callback=self.news, cb_kwargs=dict(time=time,s2=s2))
+        for link, time in zip(links, times):
+            url = response.urljoin(link)
+            yield scrapy.Request(url=url, callback=self.news, cb_kwargs=dict(name=name,time=time,s2=s2))
 
-    # def news(self, response: HtmlResponse, time,s2):
-    #     """파싱 완료 후 파이프로 전달 """
-    #     item = NewsItem()
-    #     item['name'] = '광주시청'  # db에서 가져올것
-    #     item['title'] = response.css().get(s2).strip()
-    #     item['link'] = response.url
-    #     item['time'] = time
+    def news(self, response: HtmlResponse, name,time,s2):
+        """파싱 완료 후 파이프로 전달 """
+        item = NewsItem()
+        item['name'] = name  # db에서 가져올것
+        item['title'] = response.css().get(s2).strip()
+        item['link'] = response.url
+        item['time'] = time
 
-    #     yield item
+        yield item
